b09d2d01c9dfc4ce074aa05d3ca456d10782fc45
danilvolk <danilvolk93@gmail.com>
2022-05-14 19:09:28 +0500

    Rework

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -6,7 +6,7 @@ import com.google.gson.GsonBuilder;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+
 
 public class TodoServer {
 
@@ -19,36 +19,35 @@ public class TodoServer {
     public TodoServer(int port, Todos todos) {
         this.port = port;
         this.todos = todos;
-        //...
+
     }
 
     public void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Starting server at " + port + "...");
+
         while (true) {
-            try (ServerSocket serverSocket = new ServerSocket(port);
-                 Socket socket = serverSocket.accept();
-                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            try (
+                    Socket socket = serverSocket.accept();
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
                 String input = in.readLine();
                 Task task = gson.fromJson(input, Task.class);
+
                 if (task.getType().equals("ADD")) {
                     todos.addTask(task.getTask());
                     System.out.println(todos.getAllTasks());
                     out.println(todos.getAllTasks());
-
-
                 } else if (task.getTask().equals("REMOVE")) {
                     todos.removeTask(task.getTask());
                     out.println(todos.getAllTasks());
                     System.out.println(todos.getAllTasks());
-
-
                 }
+
             } catch (IOException e) {
                 System.out.println(e.getMessage());
-
             }
-
         }
     }
 }
